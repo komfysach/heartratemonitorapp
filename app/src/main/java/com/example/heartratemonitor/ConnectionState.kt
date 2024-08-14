bdd348598dbccb8e3389ea1117806c4ff154cc10
Sachin Lendis <67464439+komfysach@users.noreply.github.com>
2024-08-14 11:43:16 +0200

    remove connection state and place into main activity

--- app/src/main/java/com/example/heartratemonitor/ConnectionState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.heartratemonitor
-
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-
-object ConnectionState {
-    var isConnected: MutableState<Boolean> = mutableStateOf(false)
-}
